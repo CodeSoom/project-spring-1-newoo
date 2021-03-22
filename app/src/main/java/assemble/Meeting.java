4479b52196a07de8cd413070823fb46df9307fdf
newoo <newoo4297@naver.com>
2021-03-22 20:52:12 +0900

    모임 도메인 객체 클래스 추가

---
+++ app/src/main/java/assemble/Meeting.java
@@ -0,0 +1,16 @@
+package assemble;
+
+/**
+ * 모임.
+ */
+public class Meeting {
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String imageUrl;
+
+    private Long ownerId;
+}
