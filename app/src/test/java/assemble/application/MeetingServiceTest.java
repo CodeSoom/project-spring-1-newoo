28dc899509ed0bbd36cd63b5766828f1d76906e0
newoo <newoo4297@naver.com>
2021-03-26 13:38:38 +0900

    모임 서비스의 모임 리스트를 반환하는 테스트를 추가하고,  통과하라.

--- app/src/test/java/assemble/application/MeetingServiceTest.java
+++ app/src/test/java/assemble/application/MeetingServiceTest.java
@@ -1,21 +1,41 @@
 package assemble.application;
 
+import assemble.domain.Meeting;
 import assemble.domain.MeetingRepository;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 @DisplayName("MeetingService의")
 class MeetingServiceTest {
+    private final Long givenId = 1L;
+    private final String givenName = "사당 iOS개발";
+    private final String givenDescription = "iOS 개발 공부해요";
+    private final Long givenOwnerId = 1L;
+
     private MeetingService meetingService;
     private MeetingRepository meetingRepository;
+    private Meeting meeting;
 
     @BeforeEach
     void setUp() {
+        meeting = Meeting.builder()
+                .id(givenId)
+                .name(givenName)
+                .description(givenDescription)
+                .ownerId(givenOwnerId)
+                .build();
+
         meetingRepository = mock(MeetingRepository.class);
         meetingService = new MeetingService(meetingRepository);
     }
@@ -26,10 +46,36 @@ class MeetingServiceTest {
         @Nested
         @DisplayName("저장된 모임이 없다면")
         class Context_without_any_meeting {
+            @BeforeEach
+            void setUp() {
+                given(meetingRepository.findAll()).willReturn(List.of());
+            }
+
             @Test
             @DisplayName("비어있는 리스트를 반환한다.")
             void it_returns_empty_list() {
                 assertThat(meetingService.getMeetings()).isEmpty();
+            }
+        }
+
+        @Nested
+        @DisplayName("저장된 모임이 있다면")
+        class Context_with_meeting {
+            private List<Meeting> givenMeetings;
+
+            @BeforeEach
+            void setUp() {
+                givenMeetings = List.of(meeting);
+
+                given(meetingRepository.findAll()).willReturn(givenMeetings);
+            }
+
+            @Test
+            @DisplayName("모임 리스트를 반환한다.")
+            void it_returns_meeting_list() {
+                assertThat(meetingService.getMeetings()).isEqualTo(givenMeetings);
+
+                verify(meetingRepository).findAll();
             }
         }
     }
