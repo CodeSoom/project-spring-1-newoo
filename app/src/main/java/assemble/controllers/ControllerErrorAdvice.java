f2fcb1fe0c9ecac983e37e88f5491cd10a108c6e
newoo <newoo4297@naver.com>
2021-04-01 20:23:59 +0900

    컨트롤러 어드바이스 클래스를 추가하라

---
+++ app/src/main/java/assemble/controllers/ControllerErrorAdvice.java
@@ -0,0 +1,18 @@
+package assemble.controllers;
+
+import assemble.errors.MeetingNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseBody
+@ControllerAdvice
+public class ControllerErrorAdvice {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(MeetingNotFoundException.class)
+    public ErrorResponse handleProductNotFound() {
+        return new ErrorResponse("Product not found");
+    }
+}
