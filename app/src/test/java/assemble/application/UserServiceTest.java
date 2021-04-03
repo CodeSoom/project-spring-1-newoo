d410c2c99d07cbe7ab0ac60599a3cead72dc1e97
newoo <newoo4297@naver.com>
2021-04-04 02:03:35 +0900

    사용자 서비스의 중복 이메일로 회원가입할 경우의 테스트를 추가하고, 통과시켜라

--- app/src/test/java/assemble/application/UserServiceTest.java
+++ app/src/test/java/assemble/application/UserServiceTest.java
@@ -5,6 +5,7 @@ import assemble.domain.RoleRepository;
 import assemble.domain.User;
 import assemble.domain.UserRepository;
 import assemble.dto.UserRegistrationData;
+import assemble.errors.UserEmailDuplicationException;
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -35,31 +37,22 @@ class UserServiceTest {
 
     private User user;
 
+    @BeforeEach
+    void setUp() {
+        Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        userService = new UserService(
+                mapper, userRepository, roleRepository, passwordEncoder);
+    }
+
     @Nested
     @DisplayName("registerUser 메서드는")
     class Describe_registerUser {
         private UserRegistrationData registrationData;
         private String givenEmail;
 
-        @BeforeEach
-        void setUp() {
-            Mapper mapper = DozerBeanMapperBuilder.buildDefault();
-            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-            userService = new UserService(
-                    mapper, userRepository, roleRepository, passwordEncoder);
-
-            given(userRepository.save(any(User.class))).will(invocation -> {
-                User source = invocation.getArgument(0);
-                return User.builder()
-                        .id(givenNewUserId)
-                        .email(source.getEmail())
-                        .name(source.getName())
-                        .build();
-            });
-        }
-
-        private void subject() throws Exception {
+        private void subject() {
             registrationData = UserRegistrationData.builder()
                     .email(givenEmail)
                     .name(givenName)
@@ -73,21 +66,52 @@ class UserServiceTest {
         @DisplayName("중복 이메일이 없다면")
         class Context_without_duplicated_email {
             @BeforeEach
-            void setUp() throws Exception {
+            void setUp() {
                 givenEmail = givenNotExistedEmail;
 
-                subject();
+                given(userRepository.save(any(User.class))).will(invocation -> {
+                    User source = invocation.getArgument(0);
+                    return User.builder()
+                            .id(givenNewUserId)
+                            .email(source.getEmail())
+                            .name(source.getName())
+                            .build();
+                });
             }
 
             @Test
             @DisplayName("새로운 사용자 정보를 생성하고, 생성된 사용자 정보를 반환한다.")
-            void it_create_new_user_and_returns_created_user() {
+            void it_create_new_user_and_returns_created_user() throws Exception {
+                subject();
+
                 assertThat(user.getId()).isEqualTo(givenNewUserId);
                 assertThat(user.getEmail()).isEqualTo(givenNotExistedEmail);
                 assertThat(user.getName()).isEqualTo(givenName);
 
                 verify(userRepository).save(any(User.class));
                 verify(roleRepository).save(any(Role.class));
+            }
+        }
+
+        @Nested
+        @DisplayName("중복 이메일이 있다면")
+        class Context_with_duplicated_email {
+            @BeforeEach
+            void setUp() throws Exception {
+                givenEmail = givenExistedEmail;
+
+                given(userRepository.existsByEmail(givenExistedEmail))
+                        .willThrow(new UserEmailDuplicationException(
+                                givenExistedEmail));
+            }
+
+            @Test
+            @DisplayName("새로운 사용자 정보를 생성하고, 생성된 사용자 정보를 반환한다.")
+            void it_create_new_user_and_returns_created_user() {
+                assertThatThrownBy(() -> subject())
+                        .isInstanceOf(UserEmailDuplicationException.class);
+
+                verify(userRepository).existsByEmail(givenExistedEmail);
             }
         }
     }
