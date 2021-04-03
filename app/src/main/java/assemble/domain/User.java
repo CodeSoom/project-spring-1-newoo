5e3d2198e8da28d291b1198ef264e1e982e70360
newoo <newoo4297@naver.com>
2021-04-03 23:00:05 +0900

    사용자 정보 도메인 객체 클래스를 추가하라

---
+++ app/src/main/java/assemble/domain/User.java
@@ -0,0 +1,34 @@
+package assemble.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 사용자 정보.
+ */
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Builder.Default
+    private String email = "";
+
+    @Builder.Default
+    private String name = "";
+
+    @Builder.Default
+    private String password = "";
+
+    @Builder.Default
+    private boolean deleted = false;
+}
