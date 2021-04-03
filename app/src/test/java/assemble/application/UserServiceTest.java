a897d13c00f3faf877583910d39be46f4797d618
newoo <newoo4297@naver.com>
2021-04-04 01:27:20 +0900

    사용자 서비스의 회원가입 테스트를 추가하라

---
+++ app/src/test/java/assemble/application/UserServiceTest.java
@@ -0,0 +1,66 @@
+package assemble.application;
+
+import assemble.domain.User;
+import assemble.domain.UserRepository;
+import assemble.dto.UserRegistrationData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@DisplayName("UserService의")
+class UserServiceTest {
+    private final String givenNotExistedEmail = "newoo@codesoom.com";
+    private final String givenExistedEmail = "existed@codesoom.com";
+    private final String givenName = "newoo";
+    private final String givenPassword = "password";
+
+    private UserService userService;
+
+    private final UserRepository userRepository = mock(UserRepository.class);
+
+    private User user;
+
+    @Nested
+    @DisplayName("registerUser 메서드는")
+    class Describe_registerUser {
+        private UserRegistrationData registrationData;
+        private String givenEmail;
+
+        private void subject() {
+            registrationData = UserRegistrationData.builder()
+                    .email(givenEmail)
+                    .name(givenName)
+                    .password(givenPassword)
+                    .build();
+
+            user = userService.registerUser(registrationData);
+        }
+
+        @Nested
+        @DisplayName("중복 이메일이 없다면")
+        class Context_without_duplicated_email {
+            @BeforeEach
+            void setUp() {
+                givenEmail = givenNotExistedEmail;
+
+                subject();
+            }
+
+            @Test
+            @DisplayName("새로운 사용자 정보를 생성하고, 생성된 사용자 정보를 반환한다.")
+            void it_create_new_user_and_returns_created_user() {
+                assertThat(user.getId()).isEqualTo(77L);
+                assertThat(user.getEmail()).isEqualTo(givenNotExistedEmail);
+                assertThat(user.getName()).isEqualTo(givenName);
+
+                verify(userRepository).save(any(User.class));
+            }
+        }
+    }
+}
